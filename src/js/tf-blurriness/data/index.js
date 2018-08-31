1d71e553903f2b777412cd837774d99948fb5022
Izzy Skye <izzy.skye@mindoktor.se>
2018-08-31 17:17:33 +0200

    Added dataset

--- src/js/tf-blurriness/data/index.js
+++ src/js/tf-blurriness/data/index.js
@@ -1,3 +1,5 @@
+// This uses the CERTH Image Blur Dataset from https://mklab.iti.gr/results/certh-image-blur-dataset/
+
 export default class Dataset {
     constructor(url){
         this.url = url;
